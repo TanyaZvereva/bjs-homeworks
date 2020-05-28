49388eb6e92f23513d44c12380414a4a8c1c2032
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2020-05-28 18:41:17 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    const S = Number(amount) - Number(contribution)
+    const today = new Date()
+    const n = (date.getFullYear() - today.getFullYear()) * 12
+    const P = Number(percent) / 12
+    console.log(P ** n)
+    const totalAmmount = S * (P + P / (((1 + P) ** n) - 1))
+
+
+    return totalAmmount
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = ""
+    console.log(name)
+    if (name === "undefined" || name === "null" || name === "") {
+        greeting = `Привет, мир!Меня зовут Аноним.`
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name} .`
+    }
+    return greeting
 }
